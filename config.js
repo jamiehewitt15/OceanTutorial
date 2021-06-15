d1fcb7472951eff4c386dd39d9cebbda0c526440
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-06-15 14:46:10 +0300

    Publishing datatoken

---
+++ config.js
@@ -0,0 +1,30 @@
+const { ConfigHelper } = require("@oceanprotocol/lib");
+const Web3 = require("web3");
+const defaultConfig = new ConfigHelper().getConfig("development");
+ 
+const urls = {
+ networkUrl: "http://localhost:8545",
+ aquarius: "http://localhost:5000",
+ providerUri: "http://localhost:8030",
+};
+ 
+const contracts = {
+ "DTFactory": "0x6F8D287a2898d7A2aff723CaD4D56aD9E94CB405",
+ "BFactory": "0xB7BA5d9a934e4b6c581af7bC76bf2cd274f5B7be",
+ "FixedRateExchange": "0x49f0960444aA0D4B35b578095aCC21Fc0678472E",
+ "Metadata": "0xC5eBD196C507f62a2F4271271E57B2F7aBD7cE82",
+ "Ocean": "0x970cE99E03d799F42db329baf74A8863d5D8380b"
+};
+ 
+const config = {
+ ...defaultConfig,
+ metadataCacheUri: urls.aquarius,
+ providerUri: urls.providerUri,
+ web3Provider: new Web3(urls.networkUrl),
+};
+ 
+module.exports = {
+ config,
+ contracts,
+ urls,
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+const Web3 = require("web3");
+const { Ocean, DataTokens } = require("@oceanprotocol/lib");
+ 
+const { factoryABI } = require("@oceanprotocol/contracts/artifacts/DTFactory.json");
+const { datatokensABI } = require("@oceanprotocol/contracts/artifacts/DataTokenTemplate.json");
+const { config, contracts, urls } = require("./config");
+ 
+ 
+ 
+const init = async () => {
+ const ocean = await Ocean.getInstance(config);
+ const blob = `http://localhost:8030/api/v1/services/consume`;
+ 
+ const accounts = await ocean.accounts.list();
+ const alice = accounts[0].id;
+ console.log('Alice account address:', alice)
+ 
+ const datatoken = new DataTokens(
+   contracts.DTFactory,
+   factoryABI,
+   datatokensABI,
+   new Web3(urls.networkUrl)
+ );
+ const tokenAddress = await datatoken.create(blob, alice);
+ console.log(`Deployed datatoken address: ${tokenAddress}`);
+};
+ 
+init();
